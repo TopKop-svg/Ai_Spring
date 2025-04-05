7f353ceb69f2596314ea9d03ab8503de15e9871e
AlexandrLikhanov <u2t1frr@gmail.com>
2025-04-06 01:31:56 +0900

    1 Бин / конфинурация xml, получение messageProvider через контекст для вывода getMessage().

--- src/main/java/org/example/ai_spring/AiSpringApplication.java
+++ src/main/java/org/example/ai_spring/AiSpringApplication.java
@@ -1,19 +1,21 @@
 package org.example.ai_spring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 
 public class AiSpringApplication {
 
     public static void main(String[] args) {
-        //ApplicationContext context = new GerGrovv
-       // SpringApplication.run(AiSpringApplication.class, args);
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         MessageService service = context.getBean(MessageService.class);
         service.printMessage(context.getBean(MessageProvider.class));
+        System.out.println("Привет");
     }
 
 }
